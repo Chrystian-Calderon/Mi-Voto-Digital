b248a79b2ccac91c8adebbc06906cbda7e699d94
ralf <chrystianralfc@gmail.com>
2025-06-17 18:04:00 -0400

    feat: Method for obtaining students in the test phase

--- src/main/java/logic/Main.java
+++ src/main/java/logic/Main.java
@@ -1,10 +1,15 @@
 package logic;
 
+import persistence.ArchivoEstudiante;
 import view.Login;
 
 public class Main {
     public static void main(String[] args) {
         Login login = new Login();
         login.setVisible(true);
+        login.setLocationRelativeTo(null);
+        
+        ArchivoEstudiante e = new ArchivoEstudiante();
+        e.getVotantes();
     }
 }
--- src/main/java/model/Estudiante.java
+++ src/main/java/model/Estudiante.java
@@ -1,5 +1,39 @@
 package model;
 
 public class Estudiante {
+    private String nombre = "";
+    private String apellidos = "";
+    private String ci = "";
+    private String carrera = "";
+    private String matricula = "";
+    private boolean habilitado = false;
+    private boolean haVotado = false;
     
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public String getApellidos() {
+        return apellidos;
+    }
+    
+    public String getCI() {
+        return ci;
+    }
+    
+    public boolean isHabilitado() {
+        return habilitado;
+    }
+    
+    public void setHabilitado(boolean habilitado) {
+        this.habilitado = habilitado;
+    }
+    
+    public boolean isHaVotado() {
+        return haVotado;
+    }
+    
+    //public void setHaVotado(MesaElectoral mesa, Frente frente) {
+    //    
+    //}
 }
